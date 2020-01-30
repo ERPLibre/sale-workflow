5d8d2575e6a2c8ad0e3315abd0585651f73b80fd
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-01-30 13:07:17 -0500

    [IMP] module sale_order_line_unlink_state_done - force delete sale order line

---
+++ sale_order_line_unlink_state_done/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2020 MathBenTech <info@mathben.tech>
+# License AGPLv3.0 or later (https://www.gnu.org/licenses/agpl-3.0.en.html).
+
+{
+    "name": "Sale Order Line unlink state done",
+    "summary": "Force unlink sale order line",
+    "category": "Sale",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "MathBenTech",
+    "website": "https://www.mathben.tech",
+    'description': """
+Sale Order Line unlink state done
+=================================
+Ignore exception when unlink sale order line.
+
+""",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+    ],
+    "demo": [],
+    "qweb": [],
+    "application": False,
+}
